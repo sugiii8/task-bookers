9d476c25f5dce146dfee35bdba49adfa1ae606ad
t.sugie <t.sugie@mof-mof.co.jp>
2020-02-23 13:45:54 +0900

    flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,21 +14,21 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     if book.save
-      flash :notice, '登録成功しました'
-      # render :show
+      flash[:notice] = '登録成功しました'
+      redirect_to books_path
     else
-      flash :alert, '登録失敗しました'
-      # render :index
+      flash[:alert] = '登録失敗しました'
+      redirect_to books_path
     end
   end
 
   def destroy
     book = Book.find(params[:id])
     if book.destroy
-      flash :notice, '削除成功しました'
+      flash[:notice] = '削除成功しました'
       # render :show
     else
-      flash :alert, '削除失敗しました'
+      flash[:alert] = '削除失敗しました'
       # render :index
     end
   end
