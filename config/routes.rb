1c185c55f42fd6cde12a291f9ab3649d57a53774
t.sugie <t.sugie@mof-mof.co.jp>
2020-02-27 06:45:54 +0900

    routing no-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'books#top'
+  root to: 'homes#show'
   resources :books
+  resource :about, only: [:show]
+  resource :home, only: [:show]
 end
