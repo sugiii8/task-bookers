7d27d4e1388b0f96a5eb9eb215a33b12145d5e01
t.sugie <t.sugie@mof-mof.co.jp>
2020-02-29 06:35:03 +0900

    user info partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#show'
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show edit]
   resources :books
   resource :about, only: [:show]
   resource :home, only: [:show]
