87564fc4caaec11555b86480fafdd055ef295506
t.sugie <t.sugie@mof-mof.co.jp>
2020-02-29 06:44:05 +0900

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit]
 
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
